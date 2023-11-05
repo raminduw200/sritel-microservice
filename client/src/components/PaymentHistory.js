18e352a5c9ade142466b0927df13956b70974b14
Ramindu Walgama <raminduw200@gmail.com>
2023-11-05 20:46:15 +0530

    chore: change history view

--- client/src/components/PaymentHistory.js
+++ client/src/components/PaymentHistory.js
@@ -1,28 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/style.css';
-import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 const bill_URL = 'http://localhost:8222/api/bill-retrieval/paid-bills';
 
 function PaymentHistory() {
-
     const [Data, setResponseData] = useState([]);
 
     useEffect(() => {
         axios.get(bill_URL)
-          .then(response => {
-            // Access the data array from the response
-            const data = response.data;
-            setResponseData(data);
-            console.log(data)
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
-      }, []);
-    
+            .then(response => {
+                const data = response.data;
+                setResponseData(data);
+                console.log(data)
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }, []);
 
     const navigate = useNavigate();
 
@@ -32,39 +28,45 @@ function PaymentHistory() {
 
     return (
         <>
-        <a href="/dashboard" className="back-to-dashboard" >Back to Dashboard</a>
-        <div className="form-container">
-            <h2>Payment History</h2>
-            <div>
-                <button onClick={navigateToBills}>View Current Bills</button>
-            </div><br />
-            <div className="form form-dashboard table-bill">
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Bill number</th>
-                                <th>Bill name</th>
-                                <th>Due date</th>
-                                <th>Amount</th>
-                                <th>Payment Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Data.map((val, key) => (
-                                <tr key={key}>
-                                    <td>{val.billNumber}</td>
-                                    <td>{val.billName}</td>
-                                    <td>{val.dueDate}</td>
-                                    <td>{val.amount}</td>
-                                    <td>{val.paidDate}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="d-flex justify-content-between mb-3">
+                            <button className="btn btn-link" onClick={() => navigate('/dashboard')}>Back to Dashboard</button>
+                            <button className="btn btn-primary" onClick={navigateToBills}>View Current Bills</button>
+                        </div>
+                        <div className="card">
+                            <h2 className="card-header text-center">Payment History</h2>
+                            <div className="card-body">
+                                <div className="table-responsive">
+                                    <table className="table">
+                                        <thead className="thead-light">
+                                        <tr>
+                                            <th>Bill number</th>
+                                            <th>Bill name</th>
+                                            <th>Due date</th>
+                                            <th>Amount</th>
+                                            <th>Payment Date</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        {Data.map((val, key) => (
+                                            <tr key={key}>
+                                                <td>{val.billNumber}</td>
+                                                <td>{val.billName}</td>
+                                                <td>{val.dueDate}</td>
+                                                <td>{val.amount}</td>
+                                                <td>{val.paidDate}</td>
+                                            </tr>
+                                        ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
